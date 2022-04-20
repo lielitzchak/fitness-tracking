c20b44ee232177c64de6fe0a7968e4a803724bc9
liel itzchak <liel.izcha321@gmail.com>
2022-04-20 15:19:54 +0300

    css compiler

--- client/src/Router/routeApp.component.tsx
+++ client/src/Router/routeApp.component.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Footer from "../Components/Layout/Footer/Footer.component";
 import Header from "../Components/Layout/Header/Header.component";
+import Gym from "../Components/Pages/Gym/Gym.component";
+import Home from "../Components/Pages/Home/Home.component";
 import Page404 from "../Components/Pages/page404/Page404.component";
 const RouteApp = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/gym" element={<Gym/>} />
         <Route path="/*" element={<Page404/>} />
       </Routes>
       <Footer />
