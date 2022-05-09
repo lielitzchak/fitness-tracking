f63d18d2c2af1f74857eb4321c4589c0b1ac310a
liel itzchak <liel.izcha321@gmail.com>
2022-05-09 09:25:48 +0300

    clean code

--- client/src/Components/Pages/AddRecipe/AddRecipe.component.tsx
+++ client/src/Components/Pages/AddRecipe/AddRecipe.component.tsx
@@ -4,10 +4,14 @@ import { PostRecipe } from "../../../service/Recipe-service";
 const AddRecipe = (): JSX.Element => {
   const [recipe, setRecipe] = useState({});
   const [min, setMin] = useState("");
+  const [messageFromServer, setMessageFromServer] = useState("");
 
   const changMinByValue = (event: any) => {
     event.preventDefault();
     event.target.value > 1 ? setMin("minutes") : setMin("minute");
+    if (event.target.value == "") {
+      setMin("");
+    }
     console.log({ ...recipe, [event.target.name]: event.target.value });
     setRecipe({ ...recipe, [event.target.name]: event.target.value });
   };
@@ -22,8 +26,8 @@ const AddRecipe = (): JSX.Element => {
     event.preventDefault();
     setRecipe(recipe);
     PostRecipe(recipe)
-      .then((response: any) => console.log(response))
-      .catch((err: any) => console.log(err));
+      .then((response: any) => setMessageFromServer(response.message))
+      .catch((err: any) => setMessageFromServer(err));
     console.log(recipe);
   };
 
@@ -32,56 +36,60 @@ const AddRecipe = (): JSX.Element => {
   //   isMilk: Boolean,
   //   HowToMake: String,
   return (
-    <form>
-      <input
-        type="text"
-        name="Name"
-        placeholder="recipe name"
-        onChange={(event) => saveValue(event)}
-      />
-      <br />
-      <input
-        type="number"
-        name="TimeToCook"
-        placeholder="time to cook"
-        onChange={(event) => changMinByValue(event)}
-      />
-      <label>{min}</label>
-      <br />
-      <label htmlFor="vegan">Is vegan?</label>
-      <input
-        type="checkbox"
-        name="vegan"
-        placeholder="vegan"
-        onChange={(event) => changMinByValue(event)}
-      />
-      <br />
-      <label htmlFor="Candy">Is Candy?</label>
-      <input
-        type="checkbox"
-        name="Candy"
-        placeholder="time to cook"
-        onChange={(event) => changMinByValue(event)}
-      />
-      <br />
-      <label htmlFor="isMilk">Is Milk?</label>
-      <input
-        type="checkbox"
-        name="isMilk"
-        placeholder="isMilk"
-        onChange={(event) => changMinByValue(event)}
-      />
-      <br />
-      <input
-        type="url"
-        name="Picture"
-        placeholder="url for Picture"
-        onChange={(event) => changMinByValue(event)}
-      />
-      <br />
-      <br />
-      <button onClick={(event) => clickOnSave(event)}>save!</button>
-    </form>
+    <>
+      <form>
+        <input
+          type="text"
+          name="Name"
+          placeholder="recipe name"
+          required
+          onChange={(event) => saveValue(event)}
+        />
+        <br />
+        <input
+          type="number"
+          name="TimeToCook"
+          placeholder="time to cook"
+          onChange={(event) => changMinByValue(event)}
+        />
+        <label>{min}</label>
+        <br />
+        <label htmlFor="vegan">Is vegan?</label>
+        <input
+          type="checkbox"
+          name="vegan"
+          placeholder="vegan"
+          onChange={(event) => changMinByValue(event)}
+        />
+        <br />
+        <label htmlFor="Candy">Is Candy?</label>
+        <input
+          type="checkbox"
+          name="Candy"
+          placeholder="time to cook"
+          onChange={(event) => changMinByValue(event)}
+        />
+        <br />
+        <label htmlFor="isMilk">Is Milk?</label>
+        <input
+          type="checkbox"
+          name="isMilk"
+          placeholder="isMilk"
+          onChange={(event) => changMinByValue(event)}
+        />
+        <br />
+        <input
+          type="url"
+          name="Picture"
+          placeholder="url for Picture"
+          onChange={(event) => changMinByValue(event)}
+        />
+        <br />
+        <br />
+        <button onClick={(event) => clickOnSave(event)}>save!</button>
+      </form>
+      {messageFromServer}
+    </>
   );
 };
 
