9e1dc1ead7093eb22b1bf949ad95123d6b70c5b5
liel itzchak <liel.izcha321@gmail.com>
2022-05-23 21:12:38 +0300

    add keys

---
+++ server/Routes/Auth-route.js
@@ -0,0 +1,7 @@
+const authRoute = require("express").Router();
+const authCtrl = require("../Controllers/Auth-controller");
+
+authRoute.post("/register", authCtrl.register);
+// authRoute.post("/login", authCtrl.login);
+
+module.exports = authRoute;
