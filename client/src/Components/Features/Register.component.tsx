116cd180a6b305445ae7c6eeb49a65b1b1be1719
liel itzchak <liel.izcha321@gmail.com>
2022-06-09 11:29:36 +0300

    add register component

--- client/src/Components/Features/Register.component.tsx
+++ client/src/Components/Features/Register.component.tsx
@@ -1,64 +1,123 @@
-import { useState } from "react";
-import { PostRegister } from "../../service/Auth-service";
+import { FormEvent } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const Register = (): JSX.Element => {
-  const [newRegister, setRegister] = useState({});
-  const [massage, setMassage] = useState("");
-  let ff = (event: any): void => {
-    setRegister({ ...newRegister, [event.target.name]: event.target.value });
-  };
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.linkedin.com/in/lielitzchak/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  const click = (event: any) => {
+const theme = createTheme();
+
+export default function Register() {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    PostRegister(newRegister)
-      .then((res) => {
-        console.log(res.message);
-        setMassage(res.message);
-        console.log(massage);
-      })
-      .catch((err) => {
-        setMassage(err);
-      });
+    const data = new FormData(event.currentTarget);
+    console.table({
+      Email: data.get("Email"),
+      Password: data.get("Password"),
+      UserName: data.get("UserName"),
+      Image: data.get("Image"),
+    });
+    
   };
 
   return (
-    <>
-      <form>
-        <input
-          name="UserName"
-          type="text"
-          placeholder="UserName"
-          onChange={(event) => ff(event)}
-        />
-        <br />
-        <input
-          name="Password"
-          type="password"
-          placeholder="Password"
-          onChange={(event) => ff(event)}
-        />
-        <br />
-        <input
-          name="Email"
-          type="email"
-          placeholder="Email"
-          onChange={(event) => ff(event)}
-        />
-        <br />
-        <input
-          name="Image"
-          type="file"
-          placeholder="Image"
-          onChange={(event) => ff(event)}
-        />
-        <br />
-        <button type="submit" onClick={(event) => click(event)}>
-          Register
-        </button>
-      </form>
-      <p>{massage}</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              // id="UserName"
+              label="Name"
+              name="UserName"
+              // autoComplete="Name"
+              // autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="Password"
+              label="Password"
+              type="Password"
+              id="Password"
+              autoComplete="current-Password"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="Email"
+              label="Email"
+              type="email"
+              id="Email"
+              autoComplete="current-email"
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              name="Image"
+              label="Image"
+              type="url"
+              id="Image"
+              autoComplete="current-Image"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default Register;
+}
